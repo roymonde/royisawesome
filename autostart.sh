9a21ab4c159d3d17688e80cd43d97a6725a74131
roymonde <roystenvaseie@gmail.com>
2018-04-15 02:21:38 +0100

    new

---
+++ autostart.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run "xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1440x0 --rotate normal --output LVDS1 --primary --mode 1440x900 --pos 0x0 --rotate normal --output VGA1 --off"
+#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+run "nm-applet"
+#run "caffeine"
+run "pamac-tray"
+run "variety"
+run "xfce4-power-manager"
+run "blueberry-tray"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+run "numlockx on"
+#run "volumeicon"
+#run "nitrogen --restore"
+run "conky -c $HOME/.config/awesome/system-overview"
+
+#run applications from startup
+#run "firefox"
+#run "atom"
+#run "dropbox"
+#run "insync start"
+#run "spotify"
